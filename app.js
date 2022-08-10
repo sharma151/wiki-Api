9811e643a854759d441d825655dd51ae3179bce6
sharma151 <sharmasaurav150503@gmail.com>
2022-08-10 20:41:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
+//TODO
+
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save();
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (req, res) {
+      if (!err) {
+        res.send("succesfully deleted");
+      } else {
+        res.send(err);
+        mon;
+      }
+    });
+  });
+
+// calling specifice data
+app
+  .route("/article/:articleTitle")
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticles) {
+        if (foundArticles) {
+          res.send(foundArticles);
+        } else {
+          res.send("not article matchinf");
+        }
+      }
+    );
+  })
+  .put(function (req, res) {
+    Article.update(
+      { title: req.params.articleTitle },
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        overwrite: true,
+      },
+      function (err) {
+        if (!err) {
+          res.send("successfully updated");
+        }
+      }
+    );
+  });
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
